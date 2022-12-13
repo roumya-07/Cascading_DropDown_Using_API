77c3faf4a93bc8925eccee88e633fb1c18750d6a
roumya.behera <roumya.behera@CSMBHUL933>
2022-12-13 12:45:57 +0530

    Adding New Project With Product Entity

---
+++ Cascading_DropDown/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cascading_DropDown.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
